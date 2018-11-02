e7429e4edd51e1b4031211a322df7a14bca43d51
nicomazz <nicomazz97@gmail.com>
2018-11-02 23:39:25 +0000

    Group service updated with basic CRUD

---
+++ group_service/GroupDAO.py
@@ -0,0 +1,28 @@
+import dataset
+
+
+class GroupDAO(object):
+    def __init__(self):
+        self.db = dataset.connect(
+            'mysql://root:not_so_secret@mysql_db/group_db')
+        self.group_table = self.db['group']
+        print("db tables:")
+        print(self.db.tables)
+
+    def get(self, group_id):
+        return self.group_table.find_one(group_id=group_id)
+
+    def get_all(self):
+        result = list(self.group_table.all())
+        print(result)
+        return result
+
+    def create(self, data):
+        self.group_table.insert(data)
+        return self.get(data['group_id'])
+
+    def update(self, group_id, data):
+        self.create( data)
+
+    def delete(self, id):
+        return self.group_table.delete(group_id=id)
--- group_service/app.py
+++ group_service/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restplus import Api, Resource, fields
 import dataset
+from GroupDAO import GroupDAO
+
 app = Flask(__name__)
 
 api = Api(app, version='1.0', title='Group service',
           description='A simple service to handle notification with firebase messaging',
           )
+'''
 config = {
         'user': 'root',
         'password': 'not_so_secret',
@@ -13,7 +16,7 @@ config = {
         'port': '3306',
         'database': 'group_db'
     }
-
+'''
 #for the docs: https://dataset.readthedocs.io/en/latest/quickstart.html
 def test_db():
     db = dataset.connect('mysql://root:not_so_secret@mysql_db/group_db')
@@ -27,125 +30,61 @@ def test_db():
     print("user list:")
     print(list(db['user'].all()))
 
-test_db()
+#test_db()
 
 
-ns = api.namespace('user', description='user management')
-notify_ws = api.namespace('notify', description='Notify service')
+group_ns = api.namespace('group', description='CRUD for groups')
+user_group_ns = api.namespace('user_group', description='Used to insert user in a group')
+group_offer = api.namespace('group', description='group management')
+ns = api.namespace('old_user', description='group management')
 
-user_id = api.model('user_id',{
-    'userId':fields.String(readOnly=True, description='The user unique identifier'),
+latlng_t = api.model("LatLng",{
+    "lat":fields.Float(),
+    "lng":fields.Float()
 })
 
-user = api.model('UserNotificationId', {
-    'userId': fields.String(readOnly=True, description='The user unique identifier'),
-    'notificationId': fields.String(required=False, description='The task details')
+offer = api.model("offer",{
+    'offerer': fields.String(description="user_id"),
+    'capacity': fields.Integer(),
+    'usersInside': fields.List(fields.String())
 })
 
-notification = api.model('notification', {
-    'title': fields.String(readOnly=True, description='Title of the notify'),
-    'body': fields.String(readOnly=True, description='body of the notify'),
-    'userTargets': fields.List(fields.Nested(user_id),required=True, description='id of users to be notified')
+group = api.model('group',{
+    'group_id':fields.Integer(),
+    'name':fields.String(description="name of the group"),
+    'path':fields.String(description="polyline encoded"),
+    'users':fields.List(fields.String()),
+    'offers':fields.List(fields.Nested(offer))
 })
 
 
-
-class UserDAO(object):
-    def __init__(self):
-        self.counter = 0
-        self.users = []
-
-    def get_all(self):
-        return self.users
-
-    def get(self, id):
-        for user in self.users:
-            if user['userId'] == id:
-                return user
-        api.abort(404, "user {} doesn't exist".format(id))
-
-    def create(self, data):
-        user = data
-        self.users.append(user)
-        return user
-
-    def update(self, id, notificationId):
-        user = self.get(id)
-        user['notificationId'] = notificationId
-        return user
-
-    def delete(self, id):
-        user = self.get(id)
-        self.users.remove(user)
+DAO = GroupDAO();
 
 
-DAO = UserDAO();
-DAO.create({'userId': 'user_1', 'notificationId':"not1"})
-DAO.create({'userId': 'user_2', 'notificationId':"not2"})
-DAO.create({'userId': 'user_3', 'notificationId':"not3"})
-DAO.create({'userId': 'user_4', 'notificationId':"not4"})
+@group_ns.route('/<int:id>')
+class Group(Resource):
+    @group_ns.doc('get info for the group with a specific id')
+    def get(self,id):
+        pass
+
+    @group_ns.doc('add or update a specific group')
+    @group_ns.expect(group)
+    #@group_ns.marshal_with(group)
+    def post(self,id):
+        return DAO.create(api.payload) , 201
 
 
-@ns.route('/list')
-class UserList(Resource):
-    '''Shows a list of all users, and lets you POST to add new tasks'''
-    @ns.doc('list_users')
-    @ns.marshal_list_with(user)
+    def delete(self,id):
+        return DAO.delete(id), 201
+
+
+@group_ns.route('/')
+class Group(Resource):
+    @group_ns.doc('get all the groups')
+  #  @group_ns.marshal_list_with(group)
     def get(self):
-        '''List all tasks'''
-        return DAO.users
+        return DAO.get_all()
 
-    # @ns.doc('create_user')
-    # @ns.expect(user)
-    # @ns.marshal_with(user, code=201)
-    # def post(self):
-    #     '''Create a new task'''
-    #     return DAO.create(api.payload), 201
-
-
-@ns.route('/<string:id>')
-@ns.response(404, 'user not found')
-@ns.param('id', 'The user id')
-class User(Resource):
-
-    '''Show a single user item and lets you delete them'''
-    @ns.doc('get_user')
-    @ns.marshal_with(user)
-    def get(self, id):
-        '''Fetch a given resource'''
-        return DAO.get(id)
-
-    @ns.doc('delete_user')
-    @ns.response(204, 'user deleted')
-    def delete(self, id):
-        '''Delete a task given its identifier'''
-        DAO.delete(id)
-        return '', 204
-
-    @ns.expect(user)
-    @ns.marshal_with(user)
-    def put(self, id):
-        '''Update a task given its identifier'''
-        print(api.payload)
-        return DAO.update(id, api.payload['notificationId'])
-    
-    @ns.doc('create_user')
-    @ns.expect(user)
-    @ns.marshal_with(user)
-    def post(self, id):
-        '''Update a task given its identifier'''
-        return DAO.create(api.payload), 201
-
-@notify_ws.route('/')
-class Notify(Resource):
-
-    
-    @notify_ws.doc('send a notification to the id in the payload')
-    @notify_ws.expect(notification)
- #   @ns.marshal_with(user)
-    def post(self, id):
-        print("todo: send a real notify")
-        return "{'status':'ok'}", 201
 
 
 if __name__ == '__main__':
