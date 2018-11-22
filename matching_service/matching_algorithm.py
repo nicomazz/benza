a9954ac864ff5a1090f355caa7ef0b276a8dbd32
nicomazz <nicomazz97@gmail.com>
2018-11-22 10:46:10 +0000

    trivial Matching algorithm implemented

--- matching_service/matching_algorithm.py
+++ matching_service/matching_algorithm.py
@@ -1,4 +1,6 @@
 import random
+import polyline
+from geopy.distance import geodesic
 
 request_example = {
    "start_location": { "lat": 12.0, "lng": 53.4}, #random point, change it to something better
@@ -10,4 +12,11 @@ group_example = {
    "path": "an encoded path following this specification: https://pypi.org/project/polyline/"
 }
 def match(request, group):
-   return random.randint(0,1001)
+   decoded_poly = polyline.decode(group["path"])
+
+   start = (request["start_location"]["lat"],request["start_location"]["lng"])
+   end = (request["end_location"]["lat"],request["end_location"]["lng"])
+
+   start_start_dist = geodesic(start,decoded_poly[0]).miles
+   end_end_dist = geodesic(end,decoded_poly[-1]).miles
+   return start_start_dist+ end_end_dist
