471fbcce99e8b1ab968e7ea8ab0317f12e8a6236
Duncan <duncanvdw@gmail.com>
2019-02-17 13:30:09 +0000

    maps work

--- benzaApp/lib/services/gmaps.dart
+++ benzaApp/lib/services/gmaps.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-/*
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: const Text('gmapstest')),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-        return new MapsDemo();
-       },
-      ),
-    ),
-  ));
-}
-*/
-
 class MapsDemo extends StatefulWidget {
-	final String name; // needed for shared transactions
-	const MapsDemo({Key key, this.name}) : super(key: key);
-	
+  final String name; // needed for shared transactions
+  const MapsDemo({Key key, this.name}) : super(key: key);
+
   @override
   MapsDemoState createState() {
-		return new MapsDemoState();
-	}
+    return new MapsDemoState();
+  }
 }
 
 class MapsDemoState extends State<MapsDemo> {
@@ -33,21 +17,27 @@ class MapsDemoState extends State<MapsDemo> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(15.0),
+      padding: EdgeInsets.all(0.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Center(
-            child: SizedBox(
-              width: 60.0,
-              height: 60.0,
-              child: GoogleMap(
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: const CameraPosition(
-                  bearing: 270.0,
-                  target: LatLng(51.5160895, -0.1294527),
-                  tilt: 30.0,
-                  zoom: 17.0,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: 300.0,
+                maxHeight: 420.0,
+                minWidth: 120.0,
+                minHeight: 120.0,
+              ),
+              child: Container(
+                margin: new EdgeInsets.all(8.0),
+                constraints: BoxConstraints.expand(),
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: const CameraPosition(
+                    target: LatLng(57.1656210, -2.1021930), //Kilau
+                    zoom: 16.0,
+                  ),
                 ),
               ),
             ),
@@ -62,5 +52,4 @@ class MapsDemoState extends State<MapsDemo> {
       mapController = controller;
     });
   }
-
 }
