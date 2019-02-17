7541a4ee67ee53d8d3ce8552caf69bbee0dbec00
Duncan <duncanvdw@gmail.com>
2019-02-17 14:23:40 +0000

    fix cursor issue, text input types

--- benzaApp/lib/pages/login/signup_page.dart
+++ benzaApp/lib/pages/login/signup_page.dart
@@ -11,6 +11,8 @@ class _SignUpPageState extends State<SignUpPage> {
   String _email;
   String _password;
 
+  bool _signUpProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -21,50 +23,59 @@ class _SignUpPageState extends State<SignUpPage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-                decoration: InputDecoration(hintText: 'Email'),
-                controller: TextEditingController.fromValue(
-                	TextEditingValue(
-                    	text: _email, selection: TextSelection.collapsed(
-                    		offset: _email.length-1
-                    	)
-                  	)
-                ),
-                onChanged: (value) {
-                  setState(() {
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                  controller: TextEditingController(text: _email),
+                  decoration: InputDecoration(hintText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  autofocus: true,
+                  onChanged: (value) {
+                    //setState(() {
                     _email = value;
-                  });
-                }),
-            SizedBox(height: 15.0),
-            TextField(
-                decoration: InputDecoration(hintText: 'Password'),
-                onChanged: (value) {
-                  setState(() {
+                    //});
+                  }),
+              SizedBox(height: 15.0),
+              TextField(
+                  controller: TextEditingController(text: _password),
+                  decoration: InputDecoration(hintText: 'Password'),
+                  obscureText: true,
+                  onChanged: (value) {
+                    //setState(() {
                     _password = value;
-                  });
-                }),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-                child: Text('Sign Up'),
-                color: Colors.blue,
-                textColor: Colors.white,
-                elevation: 7.0,
-                onPressed: () {
-                  print("Signing up a user from signup page");
-                  FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: _email, password: _password)
-                      .then((signedInUser) {
-                        UserManagement().storeNewUser(signedInUser,context);
-                  });
-                }),
-          ],
-        )),
+                    //});
+                  }),
+              SizedBox(
+                height: 10.0,
+              ),
+              _signUpProgress
+                  ? CircularProgressIndicator()
+                  : RaisedButton(
+                      child: Text('Sign Up'),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      elevation: 7.0,
+                      onPressed: () {
+                        setState(() => _signUpProgress = true); 
+                        FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: _email, password: _password)
+                            .then((FirebaseUser signedInUser) {
+                                print("\n*** Signing up a user from signup page ***\n");
+                          setState(() => _signUpProgress = false);
+                          UserManagement().storeNewUser(signedInUser, context);
+                          Navigator.of(context)
+                            ..pop()
+                            ..pushReplacementNamed('/homepage');
+                        }).catchError((e) {
+                          setState(() => _signUpProgress = false);
+                          print(e);
+                        });
+                      }),
+            ],
+          ),
+        ),
       ),
     );
   }
