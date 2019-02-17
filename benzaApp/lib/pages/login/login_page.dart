2db9fb99597758bbbfefaaf578b9cc0289d8ea2d
Duncan <duncanvdw@gmail.com>
2019-02-17 12:18:42 +0000

    overflow with keyboard fixed

--- benzaApp/lib/pages/login/login_page.dart
+++ benzaApp/lib/pages/login/login_page.dart
@@ -2,14 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String _email = 'aaa@b.com';
-  String _password = '123456';
+  String _email;
+  String _password;
 
   bool _loginInProgress = false;
 
@@ -17,69 +16,74 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Login page'),
+        title: Text('Login'),
       ),
       body: Center(
-          child: Container(
-        padding: EdgeInsets.all(50.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-                controller: TextEditingController(text: _email),
-                decoration: InputDecoration(hintText: 'Email'),
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                }),
-            SizedBox(height: 15.0),
-            TextField(
-                controller: TextEditingController(text: _password),
-                decoration: InputDecoration(hintText: 'Password'),
-                onChanged: (value) {
-                  setState(() {
-                    _password = value;
-                  });
-                }),
-            SizedBox(height: 20.0),
-            _loginInProgress ? CircularProgressIndicator() : RaisedButton(
-                child: Text('Login'),
-                color: Colors.blue,
-                textColor: Colors.white,
-                elevation: 7.0,
-                onPressed: () {
-                  //FirebaseAuth.instance.currentUser()
-                  setState(() => _loginInProgress = true);
-                  FirebaseAuth.instance
-                      .signInWithEmailAndPassword(
-                      email: _email, password: _password)
-                      .then((FirebaseUser user) {
-                    setState(() => _loginInProgress = false);
-                    //Navigator.of(context).pushReplacementNamed('/homepage');
-                  }).catchError((e) {
-                    setState(() => _loginInProgress = false);
-                    print(e);
-                  });
-                }),
-            SizedBox(
-              height: 15.0,
-            ),
-            Text('Don\'t have an account?'),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-                child: Text('Sign Up'),
-                color: Colors.blue,
-                textColor: Colors.white,
-                elevation: 7.0,
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/signup');
-                }),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(50.0),
+          child: ListView(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                  controller: TextEditingController(text: _email),
+                  decoration: InputDecoration(hintText: 'Email'),
+                  onChanged: (value) {
+                    setState(() {
+                      _email += value;
+                    });
+                  }),
+              SizedBox(height: 15.0),
+              TextField(
+                  controller: TextEditingController(text: _password),
+                  decoration: InputDecoration(hintText: 'Password'),
+                  onChanged: (value) {
+                    setState(() {
+                      _password += value;
+                    });
+                  }),
+              SizedBox(height: 15.0),
+              _loginInProgress
+                  ? CircularProgressIndicator()
+                  : RaisedButton(
+                      child: Text('Login'),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      elevation: 7.0,
+                      onPressed: () {
+                        //FirebaseAuth.instance.currentUser()
+                        setState(() => _loginInProgress = true);
+                        FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: _email, password: _password)
+                            .then((FirebaseUser user) {
+                          setState(() => _loginInProgress = false);
+                          //Navigator.of(context).pushReplacementNamed('/homepage');
+                        }).catchError((e) {
+                          setState(() => _loginInProgress = false);
+                          print(e);
+                        });
+                      }),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text('Don\'t have an account?', 
+                textAlign: TextAlign.center,
+                ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                  child: Text('Sign Up'),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  elevation: 7.0,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/signup');
+                  }),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
