d90459b73d9fc51b30e28635062d5993e2b6a028
Duncan <duncanvdw@gmail.com>
2019-02-20 14:19:01 +0000

    fixed more cursor issues

--- benzaApp/lib/pages/login/login_page.dart
+++ benzaApp/lib/pages/login/login_page.dart
@@ -7,8 +7,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String _email;
-  String _password;
+  String _email = '';
+  String _password = '';
 
   bool _loginInProgress = false;
 
@@ -25,63 +25,72 @@ class _LoginPageState extends State<LoginPage> {
             //mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               TextField(
-                  controller: TextEditingController(text: _email),
+                  //controller: TextEditingController(text: _email),
                   decoration: InputDecoration(hintText: 'Email'),
-                  keyboardType:TextInputType.emailAddress,
+                  keyboardType: TextInputType.emailAddress,
                   onChanged: (value) {
-                    //setState(() {
+                    setState(() {
                       _email = value;
-                    //});
+                    });
                   }),
-              SizedBox(height: 15.0),
+              //SizedBox(height: 15.0),
+              Text('_email: $_email'),
               TextField(
-                  controller: TextEditingController(text: _password),
+                  //controller: TextEditingController(text: _password),
                   decoration: InputDecoration(hintText: 'Password'),
                   obscureText: true,
                   onChanged: (value) {
-                    //setState(() {
+                    setState(() {
                       _password = value;
-                    //});
+                    });
                   }),
-              SizedBox(height: 15.0),
+              //SizedBox(height: 15.0),
+              Text('_password: $_password'),
+              // if the login button hasn't been pressed, display the rest of the page. If it has, display CircularProgressIndicator
               _loginInProgress
                   ? Center(child: CircularProgressIndicator())
-                  : RaisedButton(
-                      child: Text('Login'),
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      elevation: 7.0,
-                      onPressed: () {
-                        //FirebaseAuth.instance.currentUser()
-                        setState(() => _loginInProgress = true);
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: _email, password: _password)
-                            .then((FirebaseUser user) {
-                          setState(() => _loginInProgress = false);
-                          Navigator.of(context).pushReplacementNamed('/homepage');
-                        }).catchError((e) {
-                          setState(() => _loginInProgress = false);
-                          print(e);
-                        });
-                      }),
-              SizedBox(
-                height: 60.0,
-              ),
-              Text('Don\'t have an account?', 
-                textAlign: TextAlign.center,
-                ),
-              SizedBox(
-                height: 15.0,
-              ),
-              RaisedButton(
-                  child: Text('Sign Up'),
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  elevation: 7.0,
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/signup');
-                  }),
+                  : new Column(
+                      children: <Widget>[
+                        RaisedButton(
+                            child: Text('Login'),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            elevation: 7.0,
+                            onPressed: () {
+                              //FirebaseAuth.instance.currentUser()
+                              setState(() => _loginInProgress = true);
+                              FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: _email, password: _password)
+                                  .then((FirebaseUser user) {
+                                setState(() => _loginInProgress = false);
+                                Navigator.of(context)
+                                    .pushReplacementNamed('/homepage');
+                              }).catchError((e) {
+                                //setState(() => _loginInProgress = false);
+                                print(e);
+                              });
+                            }),
+                        SizedBox(
+                          height: 60.0,
+                        ),
+                        Text(
+                          'Don\'t have an account?',
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        RaisedButton(
+                            child: Text('Sign Up'),
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            elevation: 7.0,
+                            onPressed: () {
+                              Navigator.of(context).pushNamed('/signup');
+                            }),
+                      ],
+                    ),
             ],
           ),
         ),
